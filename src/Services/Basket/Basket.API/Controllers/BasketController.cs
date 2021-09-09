dce26df17fbfe5a8a40a3ae5018440704d39f87a
adisekar <adithya.kiran.sekar@gmail.com>
2021-09-09 13:43:05 -0400

    Discount api repository and UI (controllers) , and fix basket update to put

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -29,7 +29,7 @@ namespace Basket.API.Controllers
             return Ok(basket ?? new ShoppingCart(userName));
         }
 
-        [HttpPost]
+        [HttpPut]
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
