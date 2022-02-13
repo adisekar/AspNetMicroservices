27fdeee083714b70fb13a415f98f15669086f04f
adisekar <adithya.kiran.sekar@gmail.com>
2022-02-13 17:11:19 -0500

    Added eShop web client and dockerize it

---
+++ eShop/WebApps/eShop.Web/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Web.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
